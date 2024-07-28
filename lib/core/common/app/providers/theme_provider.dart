7efefaf4c788511a26563df118a83145b50fe2bc
pacolahd <mbuntangwe@gmail.com>
2024-07-28 07:42:17 +0000

    further initial setup

--- lib/core/common/app/providers/theme_provider.dart
+++ lib/core/common/app/providers/theme_provider.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:pacola_quiz/core/core/constants.dart';
+import 'package:pacola_quiz/core/utils/constants.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider with ChangeNotifier {
